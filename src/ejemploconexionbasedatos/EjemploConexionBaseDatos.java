1d34aecee0dd701ae202f6b4d0c193265ca3d7fb
cymaniatico <ingluisgarciac@gmail.com>
2019-09-11 13:59:27 -0500

    Sigan copiando estudiantes!!!

--- src/ejemploconexionbasedatos/EjemploConexionBaseDatos.java
+++ src/ejemploconexionbasedatos/EjemploConexionBaseDatos.java
@@ -11,11 +11,6 @@ package ejemploconexionbasedatos;
  */
 public class EjemploConexionBaseDatos {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+ 
     
 }
